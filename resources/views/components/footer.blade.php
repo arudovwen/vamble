f5a8936981abbed672df6e045e399deced2a1502
arudovwen <successahon@gmail.com>
2021-09-17 12:34:51 +0100

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
 <div class="content">
   <div class="row">
-    <div class="col-sm-4">
+    <div class="col-sm-3">
       <img src="/logo.png" style="width: 200px" alt="">
     </div>
       <div class="col-sm-2">
@@ -14,18 +14,29 @@
 
         </ul>
       </div>
-        <div class="col-sm-2"></div>
-          <div class="col-sm-4">
+
+          <div class="col-sm-3">
 
             <form action="post">
               <div class="form-group">
-                <label for=""></label>
-                <input type="text"
-                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                <label for="">Subscribe to our newsletter</label>
 
+                <input type="text"
+                  class="form-control" name="email" id="email" aria-describedby="helpId" placeholder="Enter your email">
+           <small id="helpId" class="form-text text-muted">Subscribe to our newsletter today to get latest news, information, promotion offers and so much more.</small>
               </div>
             </form>
           </div>
+            <div class="col-sm-3">
+              <h6>Connect with us </h6>
+              <small>Follow us on our social media platforms to get exciting offers, promotions and so much more</small>
+
+              <div class="">
+                <i class="fa fa-facebook-official mr-2" aria-hidden="true"></i>
+                <i class="fa fa-instagram mr-2" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </div>
+            </div>
   </div>
 </div>
 </footer>
